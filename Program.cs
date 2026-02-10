ca8f819760b02c17ba9f6c991ce9b853373ac3e4
Kristt27 <yambarshevy@mail.ru>
2026-02-11 00:34:26 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicFilesControlModule
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
